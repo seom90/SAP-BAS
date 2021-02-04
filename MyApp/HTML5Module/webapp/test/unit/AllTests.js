f27f5caae99aca09f2bef67644c88a74dbe3f94b
seom90 <sergio_ocando@hotmail.com>
2021-02-04 02:34:45 +0000

    week 3 finished

--- MyApp/HTML5Module/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"opensap/HTML5Module/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
---
+++ webapp/webapp/model/formatter.js
@@ -0,0 +1,29 @@
+sap.ui.define([], function () {
+	"use strict";
+
+	return {
+
+		/**
+		 * Rounds the number unit value to 2 digits
+		 * @public
+		 * @param {string} sValue the number string to be rounded
+		 * @returns {string} sValue with 2 digits rounded
+		 */
+		numberUnit : function (sValue) {
+			if (!sValue) {
+				return "";
+			}
+			return parseFloat(sValue).toFixed(2);
+		},
+		
+		formatMapUrl : function (sStreet,sZIP,sCity,sCountry){
+			
+			return "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=640x640&key=AIzaSyA2-wty07maoRLOuBF41PcxDyHp_QoFYjw&markers=" 
+			// @ts-ignore
+			+ jQuery.sap.encodeURL(sStreet + "," + sZIP + " " + sCity + "," + sCountry);
+			
+		}
+
+	};
+
+});
