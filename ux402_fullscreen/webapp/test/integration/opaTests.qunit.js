0f0ab62553476a3dd444c3eee1b87ff546bff395
Khalid MORENO <khalid.moreno@teamwork.net>
2024-09-26 21:22:15 +0000

    fullscreen program completed

---
+++ ux402_fullscreen/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["studentseom/com/sap/training/ux402/fullscreen/ux402fullscreen/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ux402_fullscreen/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"studentseomcomsaptrainingux402fullscreen/ux402_fullscreen/test/unit/controller/Carrier.controller"
+], function () {
+	"use strict";
+});
