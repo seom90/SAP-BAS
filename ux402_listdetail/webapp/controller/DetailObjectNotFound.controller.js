6f171eff713152adc2b0018a4a06176f390d99eb
Khalid MORENO <khalid.moreno@teamwork.net>
2024-09-26 21:48:40 +0000

    init appdetail

---
+++ ux402_listdetail/webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("studentseom.com.sap.training.ux402.listdetail.ux402listdetail.controller.List", {
+        onInit: function () {
+
+        }
+    });
+});
